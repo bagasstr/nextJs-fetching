e96c87a0fa136c61d2813e849a8e1947d47b3687
bagasstr <bagassatrio2805@gmail.com>
2023-08-03 22:46:56 +0700

    first commit

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server";
 import data from "../data/dataTerbaru.json";
 
 export async function GET(req) {
-  return NextResponse.json(data);
+  return new NextResponse(JSON.stringify(data));
 }
